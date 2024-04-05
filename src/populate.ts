81889a0d222053ecc000ff37be5a269b0b674380
Amejro <hansonnewton@gmail.com>
2024-04-05 14:19:32 +0100

    initial

--- src/populate.ts
+++ src/populate.ts
@@ -1,7 +1,7 @@
-import { populate } from '@vendure/core/cli';
-import { bootstrap, VendureConfig } from '@vendure/core';
-import { createConnection } from 'typeorm';
-import path from 'path';
+import { populate } from "@vendure/core/cli";
+import { bootstrap, VendureConfig } from "@vendure/core";
+import { createConnection } from "typeorm";
+import path from "path";
 
 /**
  * @description
@@ -10,31 +10,35 @@ import path from 'path';
  * function using data from the @vendure/create package.
  */
 export async function populateOnFirstRun(config: VendureConfig) {
-    const dbTablesAlreadyExist = await tablesExist(config);
-    if (!dbTablesAlreadyExist) {
-        console.log(`No Vendure tables found in DB. Populating database...`);
-        return populate(
-            () => bootstrap({
-                ...config,
-                importExportOptions: {
-                    importAssetsDir: path.join(
-                        require.resolve('@vendure/create/assets/products.csv'),
-                        '../images'
-                    ),
-                },
-                dbConnectionOptions: {...config.dbConnectionOptions, synchronize: true}
-            }),
-            require('@vendure/create/assets/initial-data.json'),
-            require.resolve('@vendure/create/assets/products.csv')
-        ).then(app => app.close())
-    } else {
-        return;
-    }
+  const dbTablesAlreadyExist = await tablesExist(config);
+  if (!dbTablesAlreadyExist) {
+    console.log(`No Vendure tables found in DB. Populating database...`);
+    return populate(
+      () =>
+        bootstrap({
+          ...config,
+          importExportOptions: {
+            importAssetsDir: path.join(
+              require.resolve("@vendure/create/assets/products.csv"),
+              "../images"
+            ),
+          },
+          dbConnectionOptions: {
+            ...config.dbConnectionOptions,
+            synchronize: true,
+          },
+        }),
+      require("@vendure/create/assets/initial-data.json"),
+      require.resolve("@vendure/create/assets/products.csv")
+    ).then((app) => app.close());
+  } else {
+    return;
+  }
 }
 
 async function tablesExist(config: VendureConfig) {
-    const connection = await createConnection(config.dbConnectionOptions);
-    const result = await connection.query(`
+  const connection = await createConnection(config.dbConnectionOptions);
+  const result = await connection.query(`
         select n.nspname as table_schema,
                c.relname as table_name,
                c.reltuples as rows
@@ -42,8 +46,7 @@ async function tablesExist(config: VendureConfig) {
         join pg_namespace n on n.oid = c.relnamespace
         where c.relkind = 'r'
               and n.nspname = '${process.env.DB_SCHEMA}'
-        order by c.reltuples desc;`
-    );
-    await connection.close();
-    return 0 < result.length;
+        order by c.reltuples desc;`);
+  await connection.close();
+  return 0 < result.length;
 }
